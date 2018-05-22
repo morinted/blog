b75b04fefa0727d48d69a09a8e82fbdc192f5391
Ted Morin <morinted@gmail.com>
2018-05-22 01:22:05 -0400

    Revert "Revert "Remove scrollreveal""
    This reverts commit f7bd6596ff1f390e9fb2c2da1cedc566a36d854b.

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -10,16 +10,4 @@ $(document).on('turbolinks:load', function (event) {
     ga('send', 'pageview')
   }
 
-  // Initialize scrollreveal
-  sr.reveal('.reveal', {
-    distance: '0',
-    duration: 500,
-    easing: 'ease-in-out',
-    origin: 'top',
-    scale: 1,
-    reset: false,
-    viewFactor: 0
-  });
 });
-
-window.sr = ScrollReveal({ reset: true });
